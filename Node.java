c7d038c95dd47009978bdc5484b8866387910d43
sxergiu <sxergiu@gmail.com>
2024-03-17 16:52:24 +0200

    initialcommit

---
+++ Node.java
@@ -0,0 +1,10 @@
+class Node {
+    Integer key;           // sorted by Key
+    Integer val;         // associated data
+    Node left, right;  // left and right subtrees
+
+    Node(Integer key, Integer val) {
+        this.key = key;
+        this.val = val;
+    }
+}
